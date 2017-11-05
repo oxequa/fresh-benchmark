c186a1e946ba5af7c734ee0c9817a60e758f6ef9
Daniele Conventi <conventi@hastega.it>
2017-11-05 11:23:45 +0100

    Removed unused framework packages.

--- libs.sh
+++ libs.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-#web_frameworks=( "default" "ace" "beego" "bone" "chi" "denco" "echov3" "fasthttp-raw" \
-#"fasthttprouter" "fasthttp-routing" "fastrouter" "gear" "gin" "goji" "gojsonrest" "gongular" "gorestful" "gorilla" "go-ozzo" "gowww" "gramework" "httprouter" \
-#"httptreemux" "lars" "lion" "martini" "neo" "pat" "pure" "r2router" "siris" "tango" "tiger" "traffic" "violetear" "vulcan")
-
 web_frameworks=( "beego" "echov3" "fresh" "gin" )
